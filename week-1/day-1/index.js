7c76a6d6b3eb2e0b6a9f4601a6a8311532d1aea6
Ricardo Becerra <becerrajricardo@gmail.com>
2026-03-25 23:53:54 -0600

    day 1; two sum and reverse string

---
+++ week-1/day-1/index.js
@@ -0,0 +1,33 @@
+function twoSum(nums, target) {
+  //tu codigo aqui
+  const map = {};
+  for(let i=0;i<nums.length;i++){
+
+    const num = nums[i];
+    const complemento = target - num;
+
+    if(map[complemento]!== undefined){
+        return [map[complemento],i];
+
+    }
+    map[num] = i;
+  }
+
+
+}
+
+
+
+function reverseString(str) {
+  // tu código aquí
+  let resultado = "";
+
+  for(let i = str.length -1; i >= 0; i--){
+    resultado += str[i];
+  }
+  return resultado;
+}
+
+// pruebas
+console.log(twoSum([2,7,11,15], 9)); // debe dar [0,1]
+console.log(reverseString("hola"));
