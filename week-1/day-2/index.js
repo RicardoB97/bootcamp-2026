b21df3b30988cef7e7b34a68e19b7d0b0861109c
Ricardo Becerra <becerrajricardo@gmail.com>
2026-03-28 15:14:27 -0600

    fix removeDuplicates

--- week-1/day-2/index.js
+++ week-1/day-2/index.js
@@ -1,14 +1,26 @@
-function containsDuplicate(nums) {
-  const set = new Set();
+// function containsDuplicate(nums) {
+//   const set = new Set();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (set.has(nums[i])) {
+//       return true;
+//     }
+//     set.add(nums[i]);
+//   }
+
+//   return false;
+// }
+
+function removeDuplicates(nums) {
+  let x = new Set();
 
   for (let i = 0; i < nums.length; i++) {
-    if (set.has(nums[i])) {
-      return true;
-    }
-    set.add(nums[i]);
+    x.add(nums[i]);
   }
 
-  return false;
+  return [...x];
 }
 
-console.log(containsDuplicate([1,2,3,1]));
+console.log(removeDuplicates([1,1,2,3,3]));
+
+//console.log(containsDuplicate([1,2,3,1]));
