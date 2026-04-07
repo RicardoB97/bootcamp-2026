17b6eb666f7a3470cc63a4b69464dd2d6c4ada2c
Ricardo Becerra <becerrajricardo@gmail.com>
2026-04-06 23:14:07 -0600

    fix: add routes file and complete API connection

--- week-1/day-4/controllers/casesController.js
+++ week-1/day-4/controllers/casesController.js
@@ -8,3 +8,81 @@ exports.getCases = (req, res) => {
     res.json(results);
   });
 };
+
+exports.createCase = (req, res) => {
+  const { client, status } = req.body;
+
+  console.log("BODY:", req.body); // 👈 DEBUG
+
+  db.query(
+    'INSERT INTO cases (client, status) VALUES (?, ?)',
+    [client, status],
+    (err, result) => {
+      if (err) {
+        console.error(err); // 👈 DEBUG
+        return res.status(500).send(err);
+      }
+
+      console.log("INSERT ID:", result.insertId); // 👈 DEBUG
+
+      res.status(201).json({
+        id: result.insertId,
+        client,
+        status
+      });
+    }
+  );
+};
+
+exports.getCaseById = (req, res) => {
+  const id = req.params.id;
+
+  db.query(
+    'SELECT * FROM cases WHERE id = ?',
+    [id],
+    (err, results) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      if (results.length === 0) {
+        return res.status(404).send("Case not found");
+      }
+
+      res.json(results[0]);
+    }
+  );
+};
+
+exports.updateCase = (req, res) => {
+  const id = req.params.id;
+  const { client, status } = req.body;
+
+  db.query(
+    'UPDATE cases SET client = ?, status = ? WHERE id = ?',
+    [client, status, id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      res.json({ message: "Case updated" });
+    }
+  );
+};
+
+exports.deleteCase = (req, res) => {
+  const id = req.params.id;
+
+  db.query(
+    'DELETE FROM cases WHERE id = ?',
+    [id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      res.json({ message: "Case deleted" });
+    }
+  );
+};
