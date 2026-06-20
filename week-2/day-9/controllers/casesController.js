92a419797ce7adda1b6ea2160063d00561fc8ff5
Ricardo Becerra <becerrajricardo@gmail.com>
2026-06-19 21:01:59 -0600

    Dia 9

--- week-2/day-9/controllers/casesController.js
+++ week-2/day-9/controllers/casesController.js
@@ -1,8 +1,31 @@
-const getCases = (req, res) => {
+const db = require('../config/db');
+
+/* const getCases = async (req, res, next) => {
+
+  try {
+
+    const [rows] = await db.query(
+      'SELECT * FROM cases'
+    );
+
+    res.json(rows);
+
+  } catch (error) {
+
+    next(error);
+
+  }
+
+}; */
+
+const getCases = async (req, res, next) => {
+
+  console.log('ENTRO AL NUEVO CONTROLLER');
+
   res.json({
-    message: 'Cases funcionando',
-    user: req.user
+    prueba: 'SI VES ESTO, ESTAMOS EN EL CONTROLLER NUEVO'
   });
+
 };
 
 const deleteCase = (req, res) => {
